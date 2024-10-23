c52dff1828e1fa3466444edf232b21cd38a8d1d9
ayush <you@example.com>
2024-10-23 21:06:24 +0530

    Initial commmit

---
+++ add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5; // First number
+    int b = 10; // Second number
+    int sum = a + b; // Calculate sum
+
+    // Print the result
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+
+    return 0;
+}
+
